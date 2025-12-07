7a5553b1e7a4c3ad74a517e29f50b2b358bca0e9
Amerish <amerishn@watancon.com>
2025-12-07 14:04:10 +0300

    workflowazure and deployment file add

--- it_ticketing/settings.py
+++ it_ticketing/settings.py
@@ -135,3 +135,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if os.environ.get("WEBSITE_HOSTNAME"):  
+    from .deployment import *
--- it_ticketing/wsgi.py
+++ it_ticketing/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+settings_module = 'it_ticketing.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'it_ticketing.settings'
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'it_ticketing.settings')
 
 application = get_wsgi_application()
